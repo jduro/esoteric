5d3868c8c3ec31b18e2f218a9489a8dce6fe7a3b
Joao Duro <joaodurodei@gmail.com>
2013-03-05 16:17:52 +0000

    first

---
+++ scrapy_project/scrapy_project/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for scrapy_project project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'scrapy_project'
+
+SPIDER_MODULES = ['scrapy_project.spiders']
+NEWSPIDER_MODULE = 'scrapy_project.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapy_project (+http://www.yourdomain.com)'
