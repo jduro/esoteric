951d33627e54625e06a894a19b594f6e1f6b5518
Joao Duro <joaodurodei@gmail.com>
2013-03-13 12:32:39 +0000

    scrapper acm - DONE

--- scrapy_project/scrapy_project/pipelines.py
+++ scrapy_project/scrapy_project/pipelines.py
@@ -13,22 +13,60 @@ class ScrapyProjectPipeline(object):
 	def __init__(self):
 		print "INIT"
 		dispatcher.connect(self.open_spider, signals.spider_opened)
-		#dispatcher.connect(self.close_spider, signals.spider_closed)
+		dispatcher.connect(self.close_spider, signals.spider_closed)
 
 	def process_item(self, item, spider):
-		print self.value
-		self.value+=1
+		# print type(str(item['title']))
+		# print type(item['title'])
 		if spider.name=="acm":
-			if 'title' in item:
-				print item['title']
-			if 'subContext' in item:
-				print "tem filhos"
+			#context=URIRef(self.USDL4EDU+str(item['title'][0].replace(" ","-").replace(":-","-").replace(",-","-"))+str(self.value))
+			context=URIRef(self.USDL4EDU+"acm"+str(self.value))
+			self.graph.add((context,RDF.type,self.concept))
+			self.graph.add((context,self.SKOS["prefLabel"],Literal(str(item['title'][0]))))
+			self.graph.add((self.acm,self.SKOS["hasTopConcept"],context))
+			if len(item['subContext'])!=0:
+				self.addContext(item['subContext'],str(self.value)+'-',context)
+			self.value+=1
 		return item
 
+	def addContext(self,items,num,father):
+		value=0
+		for item in items:
+			value+=1
+			#context=URIRef(self.USDL4EDU+str(item['title'][0].replace(" ","-").replace(":-","-").replace(",-","-"))+num+str(value))
+			context=URIRef(self.USDL4EDU+"acm"+num+str(value))
+			#O PAI TEM ESTE FILHO MAIS GENERICO
+			self.graph.add((father,self.SKOS["narrowerGeneric"],context))
+			#O FILHO TEM UM PAI MAIS ESPECIFICO
+			self.graph.add((context,self.SKOS["broaderGeneric"],father))
+			self.graph.add((context,RDF.type,self.concept))
+			self.graph.add((context,self.SKOS["prefLabel"],Literal(item['title'][0])))
+			if len(item['subContext'])!=0:
+				self.addContext(item['subContext'],num+str(value)+'-',context)
+
 	def open_spider(self,spider):
-		print "COMECOU SPIDER"
-		
-		self.value=0
+		self.value=1
+		self.graph=Graph()
+		self.initialize_uris_skos()
 		return
+
+	def close_spider(self,spider):
+		print self.graph.serialize("context.ttl",format='n3')
+		return
+
+
+	def initialize_uris_skos(self):
+		self.graph.bind('usdl4edu','http://rdf.genssiz.dei.uc.pt/usdl4edu#')
+		self.USDL4EDU = Namespace("http://rdf.genssiz.dei.uc.pt/usdl4edu#")
+
+		self.graph.bind('skos','http://www.w3.org/2004/02/skos/core#')
+		self.SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
+
+		self.concept=URIRef(self.SKOS+'Concept')
+		self.conceptScheme=URIRef(self.SKOS+'ConceptScheme')
+
+		self.acm=URIRef(self.USDL4EDU+'acm-conceptscheme')
+		self.graph.add((self.acm,RDF.type,self.conceptScheme))
+
 
    
--- scrapy_project/scrapy_project/rdf_test.py
+++ scrapy_project/scrapy_project/rdf_test.py
@@ -1,5 +1,42 @@
-from rdflib.graph import Graph
-from rdflib import Literal, BNode, Namespace
+from rdflib.graph import Graph, ConjunctiveGraph
+from rdflib import Literal, BNode, Namespace, URIRef
 from rdflib import RDF
 
-self.graph = ConjunctiveGraph()
+graph = Graph()
+#graph.parse('http://rdf.genssiz.dei.uc.pt/usdl4edu#')
+
+# graph.bind('usdl4edu','http://rdf.genssiz.dei.uc.pt/usdl4edu#')
+# USDL4EDU = Namespace("http://rdf.genssiz.dei.uc.pt/usdl4edu#")
+
+
+# online=URIRef(USDL4EDU["ModeDeliveryOnline"])
+# pres=URIRef(USDL4EDU["ModeDeliveryPres"])
+# pt=URIRef(USDL4EDU["LanguagePT"])
+# en=URIRef(USDL4EDU["LanguageEN"])
+
+
+# service=URIRef(USDL4EDU+'service1')
+# graph.add((service,RDF.type,USDL4EDU["CourseUnit"]))
+# graph.add((service,USDL4EDU["hasEcts"],Literal(6)))
+# graph.add((service,USDL4EDU["hasDeliveryMode"],online))
+# graph.add((service,USDL4EDU["hasLanguage"],pt))
+
+graph.bind('usdl4edu','http://rdf.genssiz.dei.uc.pt/usdl4edu#')
+USDL4EDU = Namespace("http://rdf.genssiz.dei.uc.pt/usdl4edu#")
+
+graph.bind('skos','http://www.w3.org/2004/02/skos/core#')
+SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
+
+concept=URIRef(SKOS+'Concept')
+conceptScheme=URIRef(SKOS+'ConceptScheme')
+
+acm=URIRef(USDL4EDU+'acm-conceptscheme')
+graph.add((acm,RDF.type,conceptScheme))
+
+print graph.serialize(format='n3')
+
+#University
+
+
+# graph.add((service,USDL4EDU["hasUniversity"],USDL4EDU["Service"]))
+# graph.add((service,RDF.type,USDL4EDU["Service"]))
