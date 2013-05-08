e3aad6e30745ccf6caf3c9b801fe1c9f15bd3d32
Joao Duro <joaodurodei@gmail.com>
2013-05-08 10:04:51 +0100

    minor changes

--- webplatform/test.rb
+++ webplatform/test.rb
@@ -8,14 +8,52 @@ service=Service.find(312)
 directory = "public/services/upload"
 path = File.join(directory, "edx.ttl")
 graph = RDF::Graph.load(service.path, :format => :ttl)
-puts graph.size
-RDF::Query.new({q: {RDF.type => USDL4EDU.EducationalService, USDL4EDU.hasCourseUnit => :unit}}).execute(graph).each do |s|
-	puts s.q.to_s
-	if s.q.to_s==service.url
-		puts s.q.to_s
-		# puts s.degree.to_s
-		puts s.unit.to_s
-	end
+# puts graph.size
+# RDF::Query.new({q: {RDF.type => USDL4EDU.EducationalService, USDL4EDU.hasCourseUnit => :unit}}).execute(graph).each do |s|
+# 	if s.q.to_s==service.url
+# 		puts s.q.to_s
+# 		# puts s.degree.to_s
+# 		puts s.unit.to_s
+
+# 		RDF::Query.new({q: {RDF.type => USDL4EDU.CourseUnit, DC.description => :description, USDL4EDU.hasDeliveryMode => :delivery, USDL4EDU.hasLanguage => :language, USDL4EDU.hasOverallObjective => :objective}}).execute(graph).each do |u|
+# 			if u.q.to_s==s.unit
+# 				puts "\n\t"+u.description.to_s
+# 				puts "\n\t"+u.delivery.to_s
+# 				puts "\n\t"+u.language.to_s
+
+# 				RDF::Query.new({q: {RDF.type => USDL4EDU.OverallObjective, DC.description => :description, USDL4EDU.hasPartObjective => :part, USDL4EDU.hasCognitiveDimension => :cognitive}}).execute(graph).each do |ob|
+# 					if ob.q.to_s==u.objective
+# 						puts "\n\t\t"+ob.description.to_s
+# 						puts "\n\t\t"+ob.part.to_s
+# 						puts "\n\t\t"+ob.cognitive.to_s
+# 					end
+# 				end
+
+# 			end
+
+# 		end
+
+# 	end
+# end
+puts "-----"
+
+query = RDF::Query.new({
+  :q => {
+    RDF.type => USDL4EDU.EducationalService,
+    USDL4EDU.hasCourseUnit => :unit
+  }
+})
+
+# query.execute(graph).each do |solution|
+# 	puts solution.q.to_s
+# 	puts "name=#{solution.unit}"
+# end
+
+solutions=query.execute(graph)
+
+solutions.filter(:q => "http://rdf.genssiz.dei.uc.pt/usdl4edu#The-Challenges-of-Global-Poverty-service").each do |solution|
+	puts solution.q.to_s
+	puts "name=#{solution.unit}"
 end
 
 
